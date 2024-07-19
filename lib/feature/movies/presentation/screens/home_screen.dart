bc4628ef9ae26b8a2c9b1fbb3b5a6dd33e74c9fc
sejan001 <sejansejan2019@gmail.com>
2024-07-19 18:13:20 +0545

    done

--- lib/feature/movies/presentation/screens/home_screen.dart
+++ lib/feature/movies/presentation/screens/home_screen.dart
@@ -24,14 +24,114 @@ class _HomeScreenState extends State<HomeScreen> {
 
     return Scaffold(
       drawer: Drawer(
+        backgroundColor:  Color.fromARGB(221, 4, 2, 2) ,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+                
+              Container(
+                height: height*.2,
+                width: width*.55,
+                decoration:BoxDecoration(
+                  image: DecorationImage(image: AssetImage('lib/assets/images/drawer.png'),fit: BoxFit.cover),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.rocket,color: Colors.white,),
+                title: Text("Discover",style: TextStyle(color: Colors.white,fontSize: 13),),
+              ),
+              SizedBox(height: height*.01,),
+                ListTile(
+                leading: Icon(Icons.movie,color: Colors.white,),
+                title: Text("Movies",style: TextStyle(color: Colors.white,fontSize: 13),),
+                
+          
+              ),
+                SizedBox(height: height*.01,),
+                ListTile(
+                leading: Icon(Icons.tv,color: Colors.white,),
+                title: Text("TV Shows",style: TextStyle(color: Colors.white,fontSize: 13),),
+                
+          
+              ),
+          
+                SizedBox(height: height*.01,),
+                ListTile(
+                leading: Icon(Icons.hd,color: Colors.white,),
+                title: Text("HD Releases",style: TextStyle(color: Colors.white,fontSize: 13),),
+                
+          
+              ),
+                SizedBox(height: height*.01,),
+              ListTile(
+              leading: Icon(Icons.calendar_month,color: Colors.white,),
+              title: Text("Calender",style: TextStyle(color: Colors.white,fontSize: 13),),
+              
+
+            ),
+              SizedBox(height: height*.01,),
+              ListTile(
+              leading: Icon(Icons.category,color: Colors.white,),
+              title: Text("Category",style: TextStyle(color: Colors.white,fontSize: 13),),
+              
+
+            ),
+            Divider(color: Colors.grey,),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Text("User",style: TextStyle(color: Colors.grey),),
+              )),
+               SizedBox(height: height*.01,),
+              ListTile(
+              leading: Icon(Icons.collections_bookmark_outlined,color: Colors.white,),
+              title: Text("Collections",style: TextStyle(color: Colors.white,fontSize: 13),),
+              
+
+            ),
+             SizedBox(height: height*.01,),
+              ListTile(
+              leading: Icon(Icons.list,color: Colors.white,),
+              title: Text("Watchlist",style: TextStyle(color: Colors.white,fontSize: 13),),
+              
+
+            ),
+           SizedBox(height: height*.01,),
+              ListTile(
+              leading: Icon(Icons.watch_later_outlined,color: Colors.white,),
+              title: Text("Recent",style: TextStyle(color: Colors.white,fontSize: 13),),
+              
+
+            ),
+             SizedBox(height: height*.01,),
+              ListTile(
+              leading: Icon(Icons.download,color: Colors.white,),
+              title: Text("Download Manager",style: TextStyle(color: Colors.white,fontSize: 13),),
+              
+
+            ),
+              Divider(color: Colors.grey,),
+          SizedBox(height: height*.01,),
+              ListTile(
+              leading: Icon(Icons.settings,color: Colors.white,),
+              title: Text("Settings",style: TextStyle(color: Colors.white,fontSize: 13),),
+              
+
+            ),
+            ],
+          ),
+        ),
         
-        width: width*.35,
+        
+        width: width*.55,
 
       ),
       
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
+            
             iconTheme: IconThemeData(color: Colors.white),
             backgroundColor: Color.fromARGB(221, 4, 2, 2),
             title: Text(
@@ -42,7 +142,9 @@ class _HomeScreenState extends State<HomeScreen> {
             floating: true,
             actions: [
               IconButton(
-                onPressed: () {},
+                onPressed: () {
+             
+                },
                 icon: Icon(
                   Icons.search,
                   color: Colors.white,
@@ -71,11 +173,14 @@ class _HomeScreenState extends State<HomeScreen> {
                   delegate: SliverChildBuilderDelegate(
                     (context, index) {
                       final movie = state.movies[index];
-                      return Container(decoration: BoxDecoration(
+                      return Container(
+                        
+                        decoration: BoxDecoration(
+                        color: Colors.black,
                         border: Border.all(color: Colors.white)
                       ),
                         child: Padding(
-                          padding: const EdgeInsets.all(4.0),
+                          padding: const EdgeInsets.all(2.0),
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
@@ -83,7 +188,7 @@ class _HomeScreenState extends State<HomeScreen> {
                                 child: CachedNetworkImage(
                                   imageUrl: movie.image.toString(),
                                   placeholder: (context, url) => Center(child: CircularProgressIndicator()),
-                                  errorWidget: (context, url, error) => Image.network("https://i.pinimg.com/736x/7f/9f/6b/7f9f6b4b5cc2de478502a18b823eb15a.jpg"),
+                                  errorWidget: (context, url, error) => Image.network("https://i.pinimg.com/736x/7f/9f/6b/7f9f6b4b5cc2de478502a18b823eb15a.jpg",fit: BoxFit.cover,),
                                   fit: BoxFit.cover,
                                   height: double.infinity,
                                   width: double.infinity,
@@ -99,6 +204,20 @@ class _HomeScreenState extends State<HomeScreen> {
                                 ),
                                 maxLines: 1,
                                 overflow: TextOverflow.ellipsis,
+                              ),
+                              Row(
+                                children: [
+                                  Text(
+                                    movie.rating.toString(),
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                  fontSize: 10
+                                    ),
+                                    maxLines: 1,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                  Icon(Icons.star,color: Colors.yellow,size: 10,)
+                                ],
                               ),
                             ],
                           ),
@@ -116,7 +235,7 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ],
       ),
-      backgroundColor: Colors.black,
+      backgroundColor: Color.fromARGB(255, 50, 49, 49),
     );
   }
 }
